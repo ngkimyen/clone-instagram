8b6970768c2b2d663a74c67a3414a4d3482387a8
Jasmine Nguyen <ngkimyen7901@gmail.com>
2021-12-10 10:41:30 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -7,24 +7,13 @@ import { BrowserRouter } from "react-router-dom";
 import LoginProvider from "./store/LoginProvider";
 
 ReactDOM.render(
-<<<<<<< HEAD
   <React.StrictMode>
-=======
->>>>>>> 74116d9099fb08d7106f6ab9d1592e7da8910975
     <BrowserRouter>
       <LoginProvider>
         <App />
       </LoginProvider>
-<<<<<<< HEAD
     </BrowserRouter>
   </React.StrictMode>,
-=======
-    </BrowserRouter>,
->>>>>>> 74116d9099fb08d7106f6ab9d1592e7da8910975
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
